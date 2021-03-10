af5f9733dba82efb60bce6569858f886913ee55e
Alphaz0r <taka.la@hotmail.com>
2021-03-10 19:48:07 +0100

    Debug menu principal boucle

--- exe.py
+++ exe.py
@@ -7,13 +7,13 @@ if __name__=="__main__":
     while(True):
         choix=launcher.Menu()
         if choix=="1":
-            client=client.Client(launcher.cnx)
+            clientDAO=client.Client(launcher.cnx)
         elif choix=="2":
-            facture=facture.Facture(launcher.cnx)
+            factureDAO=facture.Facture(launcher.cnx)
         elif choix=="3":
-            drug=drugs.Drugs(launcher.cnx)
+            drugDAO=drugs.Drugs(launcher.cnx)
         elif choix=="4":
-            concentration=concentration.Concentration(launcher.cnx)
+            concentrationDAO=concentration.Concentration(launcher.cnx)
         elif choix=="5":
-            users=users.Users(launcher.cnx)
+            usersDAO=users.Users(launcher.cnx)
     
